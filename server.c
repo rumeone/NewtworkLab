209d42298ca70e95c73d36d6a45146b47eb4ccff
rumeone <yuaroslav_29@mail.ru>
2022-02-12 22:55:13 +0700

    end server.c

--- server.c
+++ server.c
@@ -17,11 +17,55 @@ int main() {
 
     //Создаем socket для udp.
 
-    if(sockMain = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+    if((sockMain = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
         perror("Ошибка открытия socket для UDP");
         exit(1);
     }
 
+    bzero((char*) &servAddr, sizeof(servAddr));
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servAddr.sin_port = 0;
+
+    //функция bind () назначает сокету определенный адрес в семействе адресов. 
+    if (bind (sockMain, &servAddr, sizeof(servAddr))) {
+        perror("Ошибка bind");
+        exit(1);
+    }
+
+    //извлекаем номер портра и копируем порт в servAddr
+    addLenght = sizeof(servAddr);
+    if (getsockname(sockMain, &servAddr, &addLenght)) {
+        perror("Вызов getsocketname неудачен ");
+        exit(1);
+    }
+
+    printf("SERVER: номер порта: %d\n", ntohs(servAddr.sin_port));
+
+    while(1) {
+        addLenght = sizeof(clientAddr);
+        bzero(buf, BUF_SIZE);
+        if ((msgLenght = recvfrom(sockMain, buf, BUF_SIZE, 0, &clientAddr, &addLenght)) < 0) {
+            perror("Ошибка socket клиента");
+            exit(1);
+        }
+
+        printf("SERVER: Адрес клиента %s:%d\n",
+            inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
+    
+        printf("SERVER: Сообщение клиента:  \"%s\"\n", buf);
+
+        buf = strcat(buf, "World");
+
+        if(sendto(sockMain, buf, sizeof(buf), 0, (struct sockaddr *)&clientAddr, sizeof(clientAddr)) < 0 ) {
+            perror("Ошибка отправки");
+            exit(1);
+        }
+        printf("Сообщение изменено на %s\n", buf);
+    }
+    
+    close(sockMain);
 
     return 0;
 }
+
